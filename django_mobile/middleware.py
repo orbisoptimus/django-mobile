d6e72e94420ee994c65f52f12d70ccfc8ddbc633
Dongmin Jang <im@hest.kr>
2013-05-24 14:26:27 +0900

    remove exception search (iPad)

--- django_mobile/middleware.py
+++ django_mobile/middleware.py
@@ -40,16 +40,12 @@ class MobileDetectionMiddleware(object):
         'wap', 'phone', 'windows ce', 'pda', 'mobile', 'mini', 'palm',
         'netfront', 'opera mobi',
     ))
-    user_agents_exception_search = u"(?:%s)" % u'|'.join((
-        'ipad',
-    ))
     http_accept_regex = re.compile("application/vnd\.wap\.xhtml\+xml", re.IGNORECASE)
 
     def __init__(self):
         user_agents_test_match = r'^(?:%s)' % '|'.join(self.user_agents_test_match)
         self.user_agents_test_match_regex = re.compile(user_agents_test_match, re.IGNORECASE)
         self.user_agents_test_search_regex = re.compile(self.user_agents_test_search, re.IGNORECASE)
-        self.user_agents_exception_search_regex = re.compile(self.user_agents_exception_search, re.IGNORECASE)
 
     def process_request(self, request):
         is_mobile = False
@@ -58,8 +54,7 @@ class MobileDetectionMiddleware(object):
             user_agent = request.META['HTTP_USER_AGENT']
 
             # Test common mobile values.
-            if self.user_agents_test_search_regex.search(user_agent) and \
-                not self.user_agents_exception_search_regex.search(user_agent):
+            if self.user_agents_test_search_regex.search(user_agent):
                 is_mobile = True
             else:
                 # Nokia like test for WAP browsers.
